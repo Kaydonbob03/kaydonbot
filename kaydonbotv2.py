d1ffe3202212920efaa276a9d897faaf27979200
Kayden Cormier <142696941+Kaydonbob03@users.noreply.github.com>
2023-12-08 18:05:48 -0500

    Update kaydonbotv2.py

--- kaydonbotv2.py
+++ kaydonbotv2.py
@@ -1,6 +1,7 @@
 import os
 import discord
 import openai
+import json
 import requests
 from discord.ext import commands
 from discord import app_commands
@@ -14,7 +15,7 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 # Create a bot instance
 intents = discord.Intents.default()
 intents.members = True
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix=';', intents=intents)
 
 
 welcome_channels = {}
@@ -23,6 +24,8 @@ welcome_channels = {}
 @bot.event
 async def on_ready():
     # Sync the command tree
+    global welcome_channels
+    welcome_channels = await load_welcome_channels()
     await bot.tree.sync(guild=MY_GUILD)  
     print(f'Logged in as {bot.user.name} (ID: {bot.user.id})')
     print('------')
@@ -42,25 +45,52 @@ def is_admin_or_mod():
         return ctx.author.guild_permissions.administrator or \
                any(role.name.lower() in ['admin', 'moderator'] for role in ctx.author.roles)
     return app_commands.check(predicate)
-
+    
+def save_welcome_channels():
+    with open('welcome_channels.json', 'w') as file:
+        json.dump(welcome_channels, file)
+        
+async def load_welcome_channels():
+    try:
+        with open('welcome_channels.json', 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        fallback_channels = {}
+        for guild in bot.guilds:
+            welcome_channel = discord.utils.get(guild.text_channels, name='welcome')
+            if welcome_channel:
+                fallback_channels[guild.id] = welcome_channel.id
+        return fallback_channels
+        
 #Define a slash command for 'welcomeconfig'
 @bot.tree.command(name="welcomeconfig", description="Configure the welcome channel", guild=MY_GUILD)
 @is_admin_or_mod()
 async def config(interaction: discord.Interaction, channel: discord.TextChannel):
-    guild_id = interaction.guild_id
-    welcome_channels[guild_id] = channel.id
-    await interaction.response.send_message(f"Welcome channel set to {channel.mention}")
+    try:
+        guild_id = interaction.guild_id
+        welcome_channels[guild_id] = channel.id
+        save_welcome_channels() 
+        await interaction.response.send_message(f"Welcome channel set to {channel.mention}")
+    except Exception as e:
+        await interaction.response.send_message(f"Failed to set welcome channel: {e}")
 
 # Define a slash command for 'commands'
-@bot.tree.command(name="commands", description="Get a list off all commands", guild=MY_GUILD)  
-async def hello(interaction: discord.Interaction):
-    await interaction.response.send_message("""Hello! Welcome to KaydonbotV2! Here is my current commands:
-        /commands - Displays list of all commands
-        /hello - Bot will say hello
-        /chat prompt: str - This command will send whatever prompt you would like to ask the gpt api and it will return a response
-        /image prompt: str - This command will take your prompt and use DALL-E 3 image generator to generate an image 
-        /welcomeconfig - This Command is for moderators and admins to configure the welcome message channel                          
-    """)
+@bot.tree.command(name="commands", description="Get a list off all commands", guild=MY_GUILD)
+async def commands(interaction: discord.Interaction):
+    embed = discord.Embed(
+        title="KaydonbotV2 Commands",
+        description="My Prefix when not using application commands (slash commands) is ';'.",
+        color=discord.Color.yellow()
+    )
+    embed.add_field(name="/commands", value="Displays list of all commands", inline=False)
+    embed.add_field(name="/hello", value="Bot will say hello", inline=False)
+    embed.add_field(name="/chat [prompt]", value="Sends a prompt to the GPT API and returns a response", inline=False)
+    embed.add_field(name="/image [prompt]", value="Uses DALL-E 3 to generate an image based on your prompt", inline=False)
+    embed.add_field(name="/welcomeconfig", value="For moderators and admins to configure the welcome message channel", inline=False)
+    embed.set_footer(text="KaydonbotV2 - Copyright (c) @Kaydonbob03")
+
+    await interaction.response.send_message(embed=embed)
+
 
 # Define a slash command for 'hello'
 @bot.tree.command(name="hello", description="This is just a simple hello command.", guild=MY_GUILD)  
